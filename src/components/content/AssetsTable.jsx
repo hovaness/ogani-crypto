17d7a29121d9cb96df2633d083fd9964f30c740a
Prizeox <oganisyanararat1@gmail.com>
2024-02-18 13:58:01 +0400

    need upgrade

--- src/components/content/AssetsTable.jsx
+++ src/components/content/AssetsTable.jsx
@@ -1,3 +1,48 @@
-export function AssetsTable(){
-    return <></>
+import { Table } from "antd";
+import { useCrypto } from "../../context/crypto-context";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    // specify the condition of filtering result
+    // here is that finding the name started with `value`
+    sorter: (a, b) => a.name.length - b.name.length,
+    sortDirections: ["descend"],
+  },
+  {
+    title: "Price, $",
+    dataIndex: "price",
+    defaultSortOrder: "descend",
+    sorter: (a, b) => a.price - b.price,
+  },
+  {
+    title: "Amount",
+    dataIndex: "amount",
+    defaultSortOrder: "descend",
+    sorter: (a, b) => a.amount - b.amount,
+  },
+];
+
+export function AssetsTable() {
+  const { assets } = useCrypto();
+
+  const data = assets.map((asset) => ({
+    key: asset.id,
+    name: asset.name,
+    price: asset.price,
+    amount: asset.amount,
+  }))
+
+
+  return (
+    <>
+      <Table
+        //style={{display:'flex', marginBottom:'1rem', justifyContent:'center'}}
+        pagination={false}
+        columns={columns}
+        dataSource={data}
+      />
+    </>
+  );
 }
--- src/components/content/PortfolioChart.jsx
+++ src/components/content/PortfolioChart.jsx
@@ -1,38 +1,34 @@
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
+import { useCrypto } from '../../context/crypto-context';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
+
 export function PortfolioChart() {
+  const {assets} = useCrypto();
+  const data = {
+    labels: assets.map((asset) => asset.name),
+    datasets: [
+      {
+        label: '$',
+        data: assets.map((asset) => asset.totalAmount),
+        backgroundColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255,1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+      },
+    ],
+  };
   return (
     <>
-      <Pie data = {data}/>
+      <div style={{display:'flex', marginBottom:'1rem', justifyContent:'center', height:600}}>
+        <Pie data = {data}/>  
+      </div>
     </>
   );
 }
